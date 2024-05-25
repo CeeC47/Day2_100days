56adc8af2e429b155efede5e0d85a0bbaa6aafc0
CeeC47 <147382922+CeeC47@users.noreply.github.com>
2024-05-25 22:46:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+favfood = input("What is the name of your favorite food: ")
+favmusic = input("What is the anme of your favorite music: ")
+address = input("Enter your address: ")
+print("Hello", name, "your favorite food is", favfood,
+      "and your favorite music is", favmusic)
+print("Wow", name, "you are an amazing soul!")
